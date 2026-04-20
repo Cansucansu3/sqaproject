5ef525e84c5b1b473b3654df1e294bd3a07d73f1
cansucansu3 <cnsaksoy03@gmail.com>
2026-04-20 12:02:02 +0300

    Fixed BaseTest declarations and imports

--- src/test/java/com/sqa/base/BaseTest.java
+++ src/test/java/com/sqa/base/BaseTest.java
@@ -3,17 +3,33 @@ package com.sqa.base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
+import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver driver;
+    protected WebDriverWait wait; // This is the line you were missing!
 
     @BeforeEach
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        // Change to "chrome" or "firefox" as needed
+        String browser = "firefox"; 
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
         driver.manage().window().maximize();
+        
+        // NO SLEEP: This defines the global wait for the whole project
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterEach
